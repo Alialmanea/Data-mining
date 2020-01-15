bcb8b0174db6124ffa56d992bdde91f400b9ce7f
ali Rashad almanea <37377385+Alialmanea@users.noreply.github.com>
2020-01-15 17:46:22 +0300

    Create Entropy.py

---
+++ Entropy.py
@@ -0,0 +1,34 @@
+import collections
+import math
+import random
+
+def entropy(data):
+    e = 0
+
+    counter = collections.Counter(data)
+    l = len(data)
+    for count in counter.values():
+        # count is always > 0
+        p_x = count / l
+        e += - p_x * math.log2(p_x)
+
+    return e
+
+
+
+def main():
+    arr_x=[]
+    arr_y=[]
+    rnd=random
+    n=100
+    for i in range(0,n):
+        n= int(rnd.randint(1,6))
+        arr_x.append(n)
+
+    x_entropi=entropy(arr_x)
+
+    print("The Entropi   is : {}".format(x_entropi))
+
+
+if __name__ == '__main__':
+    main()
